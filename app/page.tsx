ec32ac17396defc24285c902a47f3fcede57c90a
theDarkShadow <rayyan9290@gmail.com>
2024-04-21 19:45:34 +0530

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,46 @@
 import Image from "next/image";
 
+import uberImage from "@/public/Images/uber.jpg";
+import SearchBar from "./Component/SearchBar";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Welcome to my website</h1>
-      <p className="text-lg mt-4">
-        This is a website created using Next.js and Tailwind CSS
-      </p>
-      <Image
-        src="/nextjs.svg"
-        alt="Next.js Logo"
-        width={200}
-        height={100}
-        className="mt-4"
-      />
+    <div className={`min-h-screen min-w-screen w-full h-full`}>
+      <div
+        className="min-h-screen min-w-screen w-full bg-black -z-10text-white
+      
+       lg:px-20 "
+      >
+        <div className="flex flex-col xl:flex-row xl:justify-between xl:items-center min-h-screen h-full pt-8">
+          <div className="text-white mt-8 lg:mt-4 mx-10 lg:mr-20 flex flex-col ">
+            <div className="font-helvectica text-3xl lg:text-6xl font-bold ">
+              Go anywhere with Uber
+            </div>
+
+            <SearchBar />
+          </div>
+
+          <div className="px-8 lg:px-0">
+            <Image
+              src={uberImage}
+              height={800}
+              width={900}
+              alt="Head Image"
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="min-h-screen min-w-screen w-full bg-white -z-10">How</div>
+
+      <div className="min-h-screen min-w-screen w-full bg-white -z-10">Why</div>
+
+      <div className="min-h-screen min-w-screen w-full bg-white -z-10">
+        When
+      </div>
+
+      <div className="min-h-screen min-w-screen w-full bg-white -z-10">How</div>
     </div>
   );
 }
