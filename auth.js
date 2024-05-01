850e571e8e0f5e3a96d2134f73306d2f3ebbceb9
Mohammed Huzaif Ahmed <huzaif.demha@gmail.com>
2024-05-02 00:55:58 +0530

    rayyan uddin ka chutiyapa

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ import { z } from 'zod';
 // import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 
-async function getUser(email: string): Promise<User | undefined> {
+async function getUser(email)  {
   try {
     const user = await sql < User > `SELECT * FROM users WHERE email=${email}`;
     return user.rows[0];
@@ -27,7 +27,7 @@ export const { auth, signIn, signOut } = NextAuth({
 
         if (parsedCredentials.success) {
           const { email, password } = parsedCredentials.data;
-          const user == true
+          const user = true
           if (!user) return null;
           const passwordsMatch = await bcrypt.compare(password, user.password);
 
